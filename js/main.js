1386b50ed07c39ea629054e62e9e7e4ad91c5a18
sillyboy <yyongtai@gmail.com>
2013-12-15 15:11:00 +0800

    second commit

--- js/main.js
+++ js/main.js
@@ -1,21 +1,32 @@
 var app = {
 
     findByName: function() {
-        console.log('findByName');
+        var self = this;
         this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
+            $('.employee-list').html(self.employeeLiTpl(employees));   
         });
     },
 
-    initialize: function() {
-        this.store = new MemoryStore();
+    showAlert: function (message, title) {
+        if (navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
+        } else {
+            alert(title ? (title + ":" + message) : message);
+        }
+    },
+
+    renderHomeView: function() {
+        $('body').html(this.homeTpl());
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
+        
+    },
+
+    initialize: function() {
+        var self = this;
+        this.store = new MemoryStore(function() {
+            self.renderHomeView();
+        });
+       
     }
 
 };
